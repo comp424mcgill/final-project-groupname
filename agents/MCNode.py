fdf5937884e49ed91557eb77e5d911e27c4f729e
Claire Yang <youyou.yang@mail.mcgill.ca>
2022-04-08 01:06:17 -0400

    'add expand func and check for step_number'

--- agents/MCNode.py
+++ agents/MCNode.py
@@ -1,4 +1,5 @@
 
+from matplotlib.style import available
 import numpy as np
 from copy import deepcopy
 from agents.agent import Agent
@@ -28,62 +29,101 @@ class MCNode():
     def isLeaf(self):
         return (len(self.children) == 0)
 
+
     def set_barrier(self, x, y, b):
-        # b for direction of barrier
+        # same func as Tree
+        if not 0<=b<=3:
+            return False
+
         if self.chess_board[x][y][b] == False:
             self.chess_board[x][y][b] = True
+            # neighbor's wall
+            if b == 0 :
+                self.chess_board[x][y-1][2] = True
+            elif b == 2 :
+                self.chess_board[x][y+1][0] = True
+            elif b == 1 :
+                self.chess_board[x+1][y][3] = True 
+            else:
+                self.chess_board[x-1][y][1] = True 
             return True
+
         return False
 
 
-    # tree policy here
-    def expand(self):
-        # TODO:  possible leaf to expand, if cant expand return false
-        return True 
+    def check_close_area(self, max_step=0):
+        '''
+        if reach end, return True and area of our player get
+        else, return False and available coord we can reach
+        '''
+        board = self.chess_board
+        x,y = self.coord
+        if max_step==0:
+            max_step = board.shape[0]**2//2
+        visited_map = np.zeros([board.shape[0], board.shape[1]])    
+        # if a coord is visited change it to 1
 
-
-    def check_close_area(self):
-        def _is_valid_step(chess_board, visited_map, x, y, direction):
+        def _is_valid_step(visited_map, x, y, direction):
             if direction=='l':
-                if chess_board[x][y][3]:
+                if not board[x][y][3]:
                     return False
                 x += 1  
             if direction=='r':
-                if not chess_board[x][y][1]: 
+                if not board[x][y][1]: 
                     return False
                 x -= 1  
             if direction=='u':
-                if not chess_board[x][y][0]: 
+                if not board[x][y][0]: 
                     return False
                 y -= 1  
             if direction=='d':
-                if not chess_board[x][y][2]: 
+                if not board[x][y][2]: 
                     return False
                 y += 1  
-            return visited_map[x][y]==0 and 0<=x<chess_board.shape[0] and 0<=y<chess_board.shape[1]
+            return visited_map[x][y]==0 and 0<=x<board.shape[0] and 0<=y<board.shape[1]
 
-        def area_runner(chess_board, visited_map, x, y):
+        def area_runner(visited_map, x, y, step_num):
+            step_num += 1
+            if step_num > max_step:
+                # record the steps excceed max step but still calc its area
+                visited_map[x][y] = 2
             visited_map[x][y] = 1
             area = 1
-            if _is_valid_step(chess_board, visited_map, x, y, 'l'):
-                area += area_runner(chess_board, visited_map, x+1, y)
-            if _is_valid_step(chess_board, visited_map, x, y, 'r'):
-                area += area_runner(chess_board, visited_map, x-1, y)
-            if _is_valid_step(chess_board, visited_map, x, y, 'u'):
-                area += area_runner(chess_board, visited_map, x, y-1)
-            if _is_valid_step(chess_board, visited_map, x, y, 'd'):
-                area += area_runner(chess_board, visited_map, x, y+1)
-            return area
+            if _is_valid_step(visited_map, x, y, 'l'):
+                area += area_runner(visited_map, x+1, y, step_num)[0]
+            if _is_valid_step(visited_map, x, y, 'r'):
+                area += area_runner(visited_map, x-1, y, step_num)[0]
+            if _is_valid_step(visited_map, x, y, 'u'):
+                area += area_runner(visited_map, x, y-1, step_num)[0]
+            if _is_valid_step(visited_map, x, y, 'd'):
+                area += area_runner(visited_map, x, y+1, step_num)[0]
+            return area, visited_map
 
-        board = self.chess_board
-        x,y = self.coord
-        visited_map = np.zeros([board.shape[0], board.shape[1]])    
-        # if a coord is visited change it to 1
-        
-        area = area_runner(board, visited_map, x, y)
-        # TODO: do we have to check if meet the other player? maybe not
-        if area == board.shape[0] * board.shape[1]:
-            return False, area
+        area, visited_map = area_runner(board, visited_map, x, y,0)[0]
+        available_x, available_y = np.where(visited_map==1)
+        board_area = board.shape[0] * board.shape[1]
+        if area == board_area:
+            return False, (available_x, available_y)
         return True, area
+
+
+    # tree policy here, but i dont know what's that
+    # so i just put default policy(random generate)
+    def expand(self, num_children=6):
+        # possible leaf to expand, if cant expand return false
+        reach_end, tmp = self.check_close_area()
+        if reach_end:
+            return False
+        available_x, available_y = tmp
+
+        for i in np.random.randint(0, len(available_x), 6):
+            x,y = available_x[i], available_y[i]
+            b = np.random.randint(4)
+            while not self.set_barrier(x, y, b%4):
+                b += 1
+            child = MCNode(self.chess_board, (x,y), b, parent=self)
+            self.children += [child]
+
+        return True 
 
         
--- agents/MCTree.py
+++ agents/MCTree.py
@@ -53,6 +53,27 @@ class MCTree():
                 current_node = current_node.childrenWithMaxArea()
             # TODO: use node.visit() to avoid revisiting same coord'''
 
+
+    def set_barrier(self, x, y, b):
+        # same func as node
+        if not 0<=b<=3:
+            return False
+
+        if self.chess_board[x][y][b] == False:
+            self.chess_board[x][y][b] = True
+            # neighbor's wall
+            if b == 0 :
+                self.chess_board[x][y-1][2] = True
+            elif b == 2 :
+                self.chess_board[x][y+1][0] = True
+            elif b == 1 :
+                self.chess_board[x+1][y][3] = True 
+            else:
+                self.chess_board[x-1][y][1] = True 
+            return True
+
+        return False
+
     # READ THIS
     # as ppt9 p13, for current node we generate some steps with default policy, when we reach the end we record it by backpopa. 
     # mark the success trun with visited +1 and success +1. for every depth of the tree we choose highest success/visited.
@@ -77,21 +98,25 @@ class MCTree():
         success = False
         x,y,_ = self.board_area.shape
 
+        reach_end, tmp =self.root.check_close_area()
+        if reach_end:
+            return tmp  # area
+        available_x, available_y = tmp
+
         while not success:
-            # TODO: will it beinf loop?
             other_x_next = np.random.randint(x)
             other_y_next = np.random.randint(y)
-            other_barrier_next = np.random.randint(4)
+            other_b_next = np.random.randint(4)
 
-            x_next = np.random.randint(x)
-            y_next = np.random.randint(y)
-            barrier_next = np.random.randint(4)
+            i = np.random.randint(len(available_x))
+            x_next = available_x[i]
+            y_next = available_y[i]
+            b_next = np.random.randint(4)
 
-            if (other_x_next!=x_next) and (other_y_next!=y_next) and (self.root.set_barrier(other_x_next,other_y_next,other_barrier_next)) and (self.root.set_barrier(x_next,y_next,barrier_next)):
+            collision = (other_x_next==x_next) and (other_y_next==y_next)
+            barrier_setted = (self.set_barrier(other_x_next,other_y_next,other_b_next)) and (self.set_barrier(x_next,y_next,b_next))
+            if not collision and barrier_setted:
                 success = True
-
-        # NO. TODO: this is no correct as we may go over barrier and didnt notice we reach end
-        # also, haven't consider the confict of neighbor squares sharing a barrier
 
     def backpopagation(self):
         return 0
